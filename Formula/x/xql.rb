da32abba74615d7440b95a42f8e0db5be8261dee
Rui Chen <rui@chenrui.dev>
2026-09-05 04:59:50 -0400

    xql 1.12.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/x/xql.rb
@@ -0,0 +1,21 @@
+class Xql < Formula
+  desc "Query CSV files and SharePoint Lists with SQL"
+  homepage "https://github.com/excelano/xql"
+  url "https://github.com/excelano/xql/archive/refs/tags/v1.12.0.tar.gz"
+  sha256 "f3a1b4b0663290a6e257f0d7d5e0962709c877d375d5976f2117c13f80f6ae1b"
+  license "MIT"
+  head "https://github.com/excelano/xql.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"), "./cmd/xql"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/xql --version")
+    (testpath/"input.csv").write("name\nalice\n")
+    output = shell_output("#{bin}/xql csv #{testpath}/input.csv --exec 'SELECT name' --mode=csv")
+    assert_equal "name\nalice\n", output
+  end
+end
