491b3804999ac66e6d10dd1b2efac3fcd931a0da
Rui Chen <rui@chenrui.dev>
2026-09-05 05:30:21 -0400

    swpui 0.10.1 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/s/swpui.rb
@@ -0,0 +1,22 @@
+class Swpui < Formula
+  desc "Interactive search and replacement preview"
+  homepage "https://github.com/beeb/swpui"
+  url "https://github.com/beeb/swpui/archive/refs/tags/v0.10.1.tar.gz"
+  sha256 "517a8f19498d3e5d689baabb7e48001aba81042c727010491fec27c092cd236d"
+  license any_of: ["MIT", "Apache-2.0"]
+  head "https://github.com/beeb/swpui.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    # TODO: Upstream does not expose a version command.
+    # FIXME: Replace the terminal startup failure check when upstream adds a headless preview mode.
+    output = shell_output("#{bin}/swpui </dev/null 2>&1", 101)
+    assert_match "failed to initialize terminal", output
+    assert_match "panicked", output
+  end
+end
