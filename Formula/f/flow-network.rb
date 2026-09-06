e8f66e39e60daa5135ae470d1120b5847e11cc3c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-06 14:01:05 +0000

    flow-network: add 0.2.5 bottle.

--- Formula/f/flow-network.rb
+++ Formula/f/flow-network.rb
@@ -6,6 +6,15 @@ class FlowNetwork < Formula
   license "MIT"
   head "https://github.com/programmersd21/flow.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ef6578a9c9b67c89d39fd25835060b7ea249c8706ab3c0b097830e50d5dd3565"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6cbeacc07b9d2ba51af223990d36e5717a99f0e9f654f4bbbbce39d1241bc106"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "97f327e4f2c0fbc33024d38cd0b0c34181e6a713c162a3a3e890a994e9edc43f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "29d05a1482151de68fde3468eca1d03e44cc8087408e48e3881ecca47820d822"
+    sha256 cellar: :any,                 x86_64_linux:  "a1990ff8d95244e078a1e6140be1f7cfd9362c3c713053959784be5454442ad2"
+  end
+
   depends_on "go" => :build
 
   def install
