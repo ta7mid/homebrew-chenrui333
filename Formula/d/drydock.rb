935d0d2a76d77a59d7b09c80c46e38db9aa19caf
Rui Chen <rui@chenrui.dev>
2026-09-05 20:14:30 -0400

    drydock: fix CI packaging issues
    Closes #10906.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/d/drydock.rb
+++ Formula/d/drydock.rb
@@ -7,7 +7,6 @@ class Drydock < Formula
   head "https://github.com/yetidevworks/drydock.git", branch: "main"
 
   depends_on "rust" => :build
-  depends_on "git"
 
   def install
     system "cargo", "install", *std_cargo_args(path: "crates/drydock")
