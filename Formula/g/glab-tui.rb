c603e937f2794da360ce5abdbc4613c2bb131306
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-06 14:19:39 +0000

    glab-tui: add 0.9.0 bottle.

--- Formula/g/glab-tui.rb
+++ Formula/g/glab-tui.rb
@@ -6,6 +6,15 @@ class GlabTui < Formula
   license "MIT"
   head "https://github.com/rcieri/glab-tui.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "713f9e223bd0eef3f7d2ec06c94b0a1419a0f712d44f6ec9ccba33650f9a69f5"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "79e0b0706109334c93e9776f1ed286b065738f01a02335eda1ae087d6fe55f60"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d8fc71002b148ce34d1959468e3faaefa82c820eaa18a844432f3d685753cf26"
+    sha256 cellar: :any,                 arm64_linux:   "efd14e7aeb70eabe40e04ebc6ba828cf3833150b953d83ab39d65fa3f114b0a3"
+    sha256 cellar: :any,                 x86_64_linux:  "a1f7d49e77575c8d4629ea4ab6557f5cb8b82a732bbee3fa208fd4153c48857c"
+  end
+
   depends_on "rust" => :build
   depends_on "gh"
   depends_on "glab"
