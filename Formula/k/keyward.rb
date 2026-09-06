cea054983a9d81f5e28823be32abf5eb7d09d917
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-06 13:43:11 +0000

    keyward: add 1.0.3 bottle.

--- Formula/k/keyward.rb
+++ Formula/k/keyward.rb
@@ -6,6 +6,15 @@ class Keyward < Formula
   license "MIT"
   head "https://github.com/gateway-of-last-resort/keyward.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d9d478e6d007f75ec60986bd241d3cf7f269d1493f96b950b7fd370cc54b8a85"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d9d478e6d007f75ec60986bd241d3cf7f269d1493f96b950b7fd370cc54b8a85"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d9d478e6d007f75ec60986bd241d3cf7f269d1493f96b950b7fd370cc54b8a85"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "11827dc1365b6c38a3ec94abbbd1a64607f4cb1c4d6212f7c79715f7fedcd52c"
+    sha256 cellar: :any,                 x86_64_linux:  "63c6cd009f960fc34bdaa9452198d3ef0d33c878f2bbe208904abb414a9ae9d0"
+  end
+
   depends_on "go" => :build
 
   def install
