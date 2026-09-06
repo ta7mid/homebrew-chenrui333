c8df21d25c712f6ee1e3e6989fcc2265bfca5a49
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-06 13:34:03 +0000

    tempo: add 0.1.14 bottle.

--- Formula/t/tempo.rb
+++ Formula/t/tempo.rb
@@ -6,6 +6,15 @@ class Tempo < Formula
   license "MIT"
   head "https://github.com/galaxy-io/tempo.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ce38ff3b9102c0d87662e847918c03134303155f0ab7fb0be39793dbdea3f537"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ce38ff3b9102c0d87662e847918c03134303155f0ab7fb0be39793dbdea3f537"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ce38ff3b9102c0d87662e847918c03134303155f0ab7fb0be39793dbdea3f537"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "2d87cfd2119d7203e28c3e9baa1d0a68ef009f616a3e6b4718371a5a5a9ddf0a"
+    sha256 cellar: :any,                 x86_64_linux:  "939779d6becc2d80d6634c1f309b1311adcac575b08620535872e76efeb2dd89"
+  end
+
   depends_on "go" => :build
 
   def install
