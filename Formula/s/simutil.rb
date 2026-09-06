f6c9e1ecb244547d558d6e16d5f0ec05e823b7b2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-06 15:09:27 +0000

    simutil: add 0.8.1 bottle.

--- Formula/s/simutil.rb
+++ Formula/s/simutil.rb
@@ -6,6 +6,15 @@ class Simutil < Formula
   license "MIT"
   head "https://github.com/dungngminh/simutil.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256                               arm64_tahoe:   "98b9b76b77bf1d010de762819346b15ddf0d2ddef780747610a440ca7e17547a"
+    sha256                               arm64_sequoia: "76ce1836e91678a9ddacf8fdac79b575104ce258309dc7e65bd4af955bbb569c"
+    sha256                               arm64_sonoma:  "7e0f957cd751f1b82bbea635e8c7aef4e27c00f648643bcf63cb2f280cddc2a3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "cc13b56179833d1c19df39742ee9009184acb0fd8a740b9ee155846480cc45f1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9836af18fa9c98cbe37e86a7cd583f76f900bb8f7a6f9d7aae0a94b97812cc43"
+  end
+
   depends_on "dart-sdk"
 
   def install
