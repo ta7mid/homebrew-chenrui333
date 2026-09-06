3e85d0b45bb93178215977d0f6f1efb51c791f74
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-06 12:34:11 +0000

    oyui: add 0.2.1 bottle.

--- Formula/o/oyui.rb
+++ Formula/o/oyui.rb
@@ -6,6 +6,15 @@ class Oyui < Formula
   license "GPL-3.0-only"
   head "https://github.com/emilien-jegou/oyui.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "38c1357ad114517d5f21bcd5a63526823061fc7403133939ae906e350c9a425d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a0c9ade368644490f3f85d21599ee60023774ca7e3e19768fd0249331a59af37"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c4bc8d0670f083701e9d75567f6f5b2cb36dca69bc61a08ba33806537bc9171a"
+    sha256 cellar: :any,                 arm64_linux:   "60e8f30307cc46b7bbbc0da19ff94e80d87e1de1954efffe107bf7b3539d862b"
+    sha256 cellar: :any,                 x86_64_linux:  "c57ce2b51c94b083b975a822bf91eef66d8dc59ff93f45fc316eba351aa7d37b"
+  end
+
   depends_on "rust" => :build
 
   def install
