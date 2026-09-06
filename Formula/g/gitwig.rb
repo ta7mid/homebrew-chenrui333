2421c0d8b99a2cf4c6fde32e0a055f97e0c9bb86
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-06 13:56:35 +0000

    gitwig: add 2.5.14 bottle.

--- Formula/g/gitwig.rb
+++ Formula/g/gitwig.rb
@@ -6,6 +6,15 @@ class Gitwig < Formula
   license "MIT"
   head "https://github.com/tareqmy/gitwig.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "96352a6748ff3406b4fc3326c227c6fe61c0c04e8d8cd36eba81f9546bfb0186"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "36099f5f94e97b0e3b14ee239801b66242d14ba3426fb788dedb82a838294303"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "63a12ed3909000d5312bd3a9d841fd7c7f6cacdbe8e95282b5f95716123011d8"
+    sha256 cellar: :any,                 arm64_linux:   "48ac61d5e67a22f17a7450369270ae1d216ee2d371c8d197905986ad39db63e5"
+    sha256 cellar: :any,                 x86_64_linux:  "97defb5dd0ad461259a2fa24f7dc4e7f34b6d6d6e81bee85f87530889ea46d3d"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on "libgit2"
