7f7d7840207acbdad026cf381e3a9fa003eaec1e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-06 13:16:10 +0000

    panix: add 0.9.2 bottle.

--- Formula/p/panix.rb
+++ Formula/p/panix.rb
@@ -6,6 +6,15 @@ class Panix < Formula
   license "AGPL-3.0-only"
   head "https://github.com/mihakrumpestar/panix.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6e9f18fb953475e3fc11f9a14c2c8710390ee0b4628ad7bdaa2488fe2e27bdaa"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6e9f18fb953475e3fc11f9a14c2c8710390ee0b4628ad7bdaa2488fe2e27bdaa"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6e9f18fb953475e3fc11f9a14c2c8710390ee0b4628ad7bdaa2488fe2e27bdaa"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "b3212a670f4ba16b890493509725ff1a92bee2b405b329a7cf1a49a95bb6c20f"
+    sha256 cellar: :any,                 x86_64_linux:  "d8d2d173930b97ab6f2e571bf74fc7372d3775078ee3b2922c49d473c680b04c"
+  end
+
   depends_on "go" => :build
 
   def install
