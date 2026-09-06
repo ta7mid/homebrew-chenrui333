7e2c0bf7ae65f541eeabfc271616b5246a6f9c6c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-06 12:38:43 +0000

    regexplain: add 1.0.0 bottle.

--- Formula/r/regexplain.rb
+++ Formula/r/regexplain.rb
@@ -6,6 +6,15 @@ class Regexplain < Formula
   license "MIT"
   head "https://github.com/kapilpokhrel/regexplain.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "af3264fabf297f2fdebf19b365a7ef1c0cda87bc76a0caa3d813ed00b69e2900"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b6e66fc302c2d42a02778523a730d28492240122c5f6d927903bd02fbeb16748"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "08efae144c3ef8ce3f2b68e1467ae9bb0e5e66f059f6061e0560bdc9c860e0b1"
+    sha256 cellar: :any,                 arm64_linux:   "f15c3e80d02563acbb213a35782e353b79ada23d67885df24152a9be7c441e62"
+    sha256 cellar: :any,                 x86_64_linux:  "5d09908d8e8e267e2f9d1ddf12ff4a8d98a729b09ce6380260c0c6e05b132f96"
+  end
+
   depends_on "rust" => :build
 
   def install
