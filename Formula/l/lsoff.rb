f88d83eaad783ad377f327e17e8f36f3af451994
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-06 14:10:40 +0000

    lsoff: add 0.1.4 bottle.

--- Formula/l/lsoff.rb
+++ Formula/l/lsoff.rb
@@ -6,6 +6,15 @@ class Lsoff < Formula
   license "MIT"
   head "https://github.com/yutat23/lsoff.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "43ed99253b1ecadc51532167f88c5eab1b0b6d8d077a6f984142bb967b0de74e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "eafa1c1b71a5dbb96e46a6ff56c3bfd20e75318b9db4ea6f9fc02a87d850eaa6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "115258613b26164b568fa0cf377398c5e1743a9a7edc2e0c4351a9cf5e859d89"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "448c7c5e499ffde915b7c26ae45b568cf57a040be028912b94d46beef3466068"
+    sha256 cellar: :any,                 x86_64_linux:  "316feb7670302b3c61f89cb4dca8d067a72f9f2745b8e552b3c8913998cd6776"
+  end
+
   depends_on "go" => :build
 
   def install
