ef7b8516e4b2fcc664987e0ce799b1d764e8058a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-06 12:20:56 +0000

    kmon: add 1.7.1 bottle.

--- Formula/k/kmon.rb
+++ Formula/k/kmon.rb
@@ -5,6 +5,12 @@ class Kmon < Formula
   sha256 "fd8e02c17089e88c2b019e116e0b7fdd9fe4285327bd795de90622aba4b79469"
   license "GPL-3.0-only"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any, arm64_linux:  "3e116256290d9815fbc17a33516d11f452201c8b10d71d79ecd1dc119c7cbdfc"
+    sha256 cellar: :any, x86_64_linux: "6fc3265a0780dc4825a3b57114b1ef31c406803550d82e39fe7443918985d981"
+  end
+
   depends_on "rust" => :build
   depends_on :linux
 
