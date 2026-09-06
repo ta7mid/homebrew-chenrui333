16e4ccf6f18144a4b27eb14dea023895c57f20a8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-06 14:38:38 +0000

    ku: add 0.11.0 bottle.

--- Formula/k/ku.rb
+++ Formula/k/ku.rb
@@ -6,6 +6,15 @@ class Ku < Formula
   license "MIT"
   head "https://github.com/bjarneo/ku.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1ba4a671da648e49bf6c9ab58bbdc262b12d93d2d4b455fb3c5efd21c3cb0635"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1ba4a671da648e49bf6c9ab58bbdc262b12d93d2d4b455fb3c5efd21c3cb0635"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1ba4a671da648e49bf6c9ab58bbdc262b12d93d2d4b455fb3c5efd21c3cb0635"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "bc96e6c5edf365c9df9e89cdc1a180cb598ccbdc28d3a8a60f9daa54f7d155a7"
+    sha256 cellar: :any,                 x86_64_linux:  "e5e7e0e1434bb0b0a32888849684ff040587ce308f2cbe82c7f04cc1527bb1b7"
+  end
+
   depends_on "go" => :build
 
   def install
