6908aacd2f7da2c19d432ada15f43d611bf2f179
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-06 14:06:11 +0000

    xql: add 1.12.0 bottle.

--- Formula/x/xql.rb
+++ Formula/x/xql.rb
@@ -6,6 +6,15 @@ class Xql < Formula
   license "MIT"
   head "https://github.com/excelano/xql.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d7bf73478d9eece672475d6f6a57df92c5d761eb70b611c5ee80a74302acd2ed"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d7bf73478d9eece672475d6f6a57df92c5d761eb70b611c5ee80a74302acd2ed"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d7bf73478d9eece672475d6f6a57df92c5d761eb70b611c5ee80a74302acd2ed"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1508b92103c68df2fcafef27c1a07c246d2f4c5d80a4369cdf82fa9a22c97451"
+    sha256 cellar: :any,                 x86_64_linux:  "3d79fe771955243fa0e8fe0fa189804a2817efa7ce502dd5640435141b1fc165"
+  end
+
   depends_on "go" => :build
 
   def install
