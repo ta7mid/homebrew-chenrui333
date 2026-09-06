7bdebbcc2b62705526f6b9429aa8f40881b4b21e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-06 12:56:16 +0000

    ttt-editor: add 1.4.0 bottle.

--- Formula/t/ttt-editor.rb
+++ Formula/t/ttt-editor.rb
@@ -6,6 +6,15 @@ class TttEditor < Formula
   license "MIT"
   head "https://github.com/eugenioenko/ttt.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "50d18c52717ec5f086b786bcfdec5ba5a6fb1b2a74110cc09296f6959efed93d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "50d18c52717ec5f086b786bcfdec5ba5a6fb1b2a74110cc09296f6959efed93d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "50d18c52717ec5f086b786bcfdec5ba5a6fb1b2a74110cc09296f6959efed93d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "0dd2f9c458b831065b5e4a3a4a6bf690c5f9257d34e349bf3ae0a7cdc236119e"
+    sha256 cellar: :any,                 x86_64_linux:  "a3802ec386156e9df39d5372663a66f5374c91e264ae57c31709ec9fb70056a3"
+  end
+
   depends_on "go" => :build
 
   def install
